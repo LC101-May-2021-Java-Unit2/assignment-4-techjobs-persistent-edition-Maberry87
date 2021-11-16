ae67c940672426058d1d0bdd83dcaaf3b741655a
Maberry87 <michael123087@gmail.com>
2021-11-15 20:43:03 -0600

    Working on controllers. need to fix some things to get app running again

--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -1,5 +1,29 @@
 package org.launchcode.techjobs.persistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Skill extends AbstractEntity {
+    @NotNull(message = "Description Required")
+    @Size(max = 300)
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Skill (String aDescription) {
+        super();
+        this.description = aDescription;
+    }
+
+    public Skill() {};
+
 
 }
